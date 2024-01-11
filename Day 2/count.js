af58cb4301f308d2f236d4490ae6a844f9622e38
Ishwor <git.ishwor@gmail.com>
2024-01-11 15:43:20 -0700

    learning js commansd

---
+++ Day 2/count.js
@@ -0,0 +1,17 @@
+const myObject =  {
+    property : 'value',
+    "obnoxious property" : function () {
+
+    }
+};
+
+// Dot notation
+myObject.property;
+
+// Broke notaion 
+myObject["obnoxious property"];
+
+
+function printName(player) {
+    console.log(player.name);
+} 
