8526c107781454b2f4fe03672079a65638ba2ab0
Ishwor <git.ishwor@gmail.com>
2024-01-16 13:55:48 -0700

    learning variable types

---
+++ Day 4/index.js
@@ -0,0 +1,7 @@
+const a = "one plus eight"
+const b = "22 times three"
+const c = "the *remainder* of 5/4"
+const d = "the variable 'a' minus 17"
+const e = "the sum of the previous four variables"
+
+module.exports = {a, b, c, d, e}
