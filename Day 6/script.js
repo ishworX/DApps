cef7237058af4f204a8d54998b4bf3a883d58bc9
ishworX <git.ishwor@gmail.com>
2024-01-18 23:45:06 -0700

    pseudo code

---
+++ Day 6/script.js
@@ -0,0 +1,23 @@
+// Pseudo Code:
+// Playround will compare the chocies from player and computer
+// It will retiurn a choice, update scores for both compueter and player
+
+// getRandomChoice() it will get assigned to rock paper and scissor
+
+// updateChoice assigns emojis to computer
+
+// updateScore()  gives output
+
+// updateScoreMessage (winner, playerselection, computerselection) display texts
+
+// restartgame()
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function getComputerChoice()
+console.log(playRound(playerSelection, computerSelection));
