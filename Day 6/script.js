d32a5778ee190dc8eb6ba0a7de3e791ff9912944
ishworX <git.ishwor@gmail.com>
2024-01-20 23:51:47 -0700

    removed unnecessary comment

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,10 +1,3 @@
-// Start a new Git repo for your project.
-
-// Create a blank HTML document with a script tag (Hint: it is best practice to link an external .js file). 
-// This game is going to be played completely from the console, so don’t worry about putting anything else in there.
-// Your game is going to play against the computer, so begin with a function called getComputerChoice that will 
-// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. 
-// Tip: use the console to make sure this is returning the expected output before moving to the next step!
 function getComputerChoice() {
     // Assign Rock Paper and Scissor
     let randomChoice = Math.floor(Math.random() * 3);
