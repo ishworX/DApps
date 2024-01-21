27cca58c608f73784b62f451d8ae9fff5ae4fc0a
ishworX <git.ishwor@gmail.com>
2024-01-20 23:34:23 -0700

    Variable Initiallization

--- Day 6/script.js
+++ Day 6/script.js
@@ -13,7 +13,12 @@ function getComputerChoice() {
 // Write a function that plays a single round of Rock Paper Scissors. The function should take 
 // two parameters - the playerSelection and computerSelection - 
 // and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+let playerscore = 0;
+let computerscore = 0;
+let gameSituation = ' ';
+
 function playRound(playerSelection, computerSelection) {
+
     
 }
 
