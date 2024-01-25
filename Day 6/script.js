aa08588b5bf02c821fc8c5643f9869acd262b92d
ishworX <git.ishwor@gmail.com>
2024-01-24 22:08:31 -0700

    Interation functions

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,3 +1,28 @@
+//For Interaction
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn =document.getElementById('rockBtn')
+const paperBtn =document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
+//Interaction Function
+
+rockBtn.addEventListener('click',() => handleClick('ROCK'))
+paperBtn.addEventListener('click', () => handleClick('PAPER'))
+scissorsBtn.addEventListener('click', () =>handleClick('SCISSORS'))
+restartBtn.addEventListener('click',restartGame)
+overlay.addEventListener('click',closeEndgameModal)
+
+
 function getComputerChoice() {
     // Assign Rock Paper and Scissor
     let randomChoice = Math.floor(Math.random() * 3);
