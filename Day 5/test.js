98da40722326ea0fa245bc6d62b1083bf92bbadf
ishworX <git.ishwor@gmail.com>
2024-01-30 23:47:22 -0700

    making both the choices smaller

--- Day 5/test.js
+++ Day 5/test.js
@@ -1,22 +1,25 @@
 const prompt = require("prompt-sync")();
 
-let inp = prompt(`Please enter your choice: Rock, Paper Or Scissor: `);
-let choice = inp.toLowerCase();
 
-console.log(`Your input is ${choice}.`);
 
 let computerScore = 0;
 let humanScore = 0;
 let result = '';
 
+function getUserChoice() {
+    let inp = prompt(`Please enter your choice: Rock, Paper Or Scissor: `);
+    let choice = inp.toLowerCase();
+    
+    return choice;
+}
 
 function getComputerChoice() {
     const compChoice = ["Rock", "Paper", "Scissor"];
 
     const random = Math.floor(Math.random() * compChoice.length);
-    return compChoice[random];
+    return compChoice[random].toLocaleLowerCase();
 }
-
+console.log(getUserChoice());
 console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
