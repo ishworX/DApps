2f00db0d2b0f45b1eb113d09781520089f9c7911
ishworX <git.ishwor@gmail.com>
2024-02-02 02:08:15 -0700

    finished playRound function where it checks if the human and computer win Condition!

--- Day 6/script.js
+++ Day 6/script.js
@@ -15,3 +15,24 @@ function getComputerChoice() {
 let user = getUserChoice();
 let comp = getComputerChoice();
 console.log(user + ' and ' + comp);
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return "TIE, Try again!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return "You Won! Rock beats Scissor.";
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return "You Won! Scissor beats Paper.";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You Won! Paper beats Rock.";
+    } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
+        return "You Lost! Rock beats Scissor.";
+    } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
+        return "You Lost! Scissor beats Paper.";
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return "You Lost! Paper beats Rock.";
+    } else {
+        return "Please enter a valid input!";
+    }
+} 
