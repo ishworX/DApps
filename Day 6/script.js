48711466215977f3008ec044b90fb3ec4b46a1ad
ishworX <git.ishwor@gmail.com>
2024-02-02 01:56:11 -0700

    testing user and computer choice function

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,1 +1,14 @@
-let input = prompt(`Please enter your choice: Rock, Paper Or Scissor: `)
+// Function : Get's user's input.
+function getUserChoice() {
+    let input = prompt(`Please enter your choice: Rock, Paper Or Scissor: `)
+    return input.toLowerCase();
+}
+
+// Function : Get's computer's choice.
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissor'];
+
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return months[randomChoice].toLowerCase();
+}
+
