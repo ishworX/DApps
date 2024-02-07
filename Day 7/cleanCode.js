d44a611cad7eeacaa0a4087400fc40405c6041b0
ishworX <git.ishwor@gmail.com>
2024-02-06 23:11:09 -0700

    use of good naming Variables

--- Day 7/cleanCode.js
+++ Day 7/cleanCode.js
@@ -9,3 +9,21 @@ function fetchPlayerName();
 function retrievePlayer1Tag();
 
 
+// Good
+const numberOfThings = 10;
+const myName = "Thor";
+const selected = true;
+
+// Bad (these start with verbs, could be confused for functions)
+const getCount = 10;
+const isSelected = true;
+
+// Good
+function getCount() {
+    return numberOfThings;
+}
+
+// Bad (it's a noun)
+function myName() {
+    return "Thor";
+}
