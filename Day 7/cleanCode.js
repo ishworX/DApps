32bdd57893f906d2c42ae471a320285a2afd8057
ishworX <git.ishwor@gmail.com>
2024-02-06 23:13:23 -0700

    Writing better comments!

--- Day 7/cleanCode.js
+++ Day 7/cleanCode.js
@@ -27,3 +27,41 @@ function getCount() {
 function myName() {
     return "Thor";
 }
+
+// This line is a bit too long
+let reallyReallyLongLine = something + somethingElse + anotherThing + howManyTacos + oneMoreReallyLongThing;
+
+// You could format it like this
+let reallyReallyLongLine =
+    something +
+    somethingElse +
+    anotherThing +
+    howManyTacos +
+    oneMoreReallyLongThing;
+
+// Or maybe like this
+let anotherReallyReallyLongLine = something + somethingElse + anotherThing +
+    howManyTacos + oneMoreReallyLongThing;
+
+// Bad Example - comment doesn't tell why, only what and how
+
+// This function increments the value of i by 1
+function incrementI(i) {
+    i = i + 1; // Add one to i
+    return i;
+}
+
+// Better Example - comment tells a why
+
+// This function increments the value of index to move to the next element
+function incrementI(i) {
+    i = i + 1;
+    return i;
+}
+
+// Good Example - the code tells all that is needed
+
+function moveToNextElement(index) {
+    index = index + 1;
+    return index;
+}
