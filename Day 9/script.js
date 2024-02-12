98d917583bda8a8becd27e8bc562da9c5ab0d435
ishworX <git.ishwor@gmail.com>
2024-02-11 23:55:54 -0700

    making a grid

---
+++ Day 9/script.js
@@ -0,0 +1,12 @@
+const gridSize = document.querySelector("#gridsize");
+
+let side = 16;
+let stylus = "pencil";
+
+gridSize.addEventListener("click", () => {
+    let size = prompt("Enter a grid dimension between 1 and 100:", side);
+    if (!Number.isNaN(size) && +size >= 1 && +size <= 100) {
+        side = +size;
+        createGrid(+size);
+    }
+})
